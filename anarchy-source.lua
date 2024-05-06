6428abe97e1ac8dc5b2fadeb8d8a88d447a3fca0
anarchy-solutions <164175212+anarchy-solutions@users.noreply.github.com>
2024-05-06 18:08:49 +0200

    Update anarchy-source.lua

--- anarchy-source.lua
+++ anarchy-source.lua
@@ -1,7 +1,19 @@
-while true do
 game:GetService("StarterGui"):SetCore("SendNotification",{
     Title = "Anarchy V2 | Light Mode", -- Required
     Text = "This version is made for free exploits.", -- Required
         Duration = "5" -- Optional
 })
-end
+task.wait(2)
+game:GetService("StarterGui"):SetCore("SendNotification",{
+    Title = "Anarchy V2 | Light Mode", -- Required
+    Text = "Loading Libraries...", -- Required
+        Duration = "5" -- Optional
+})
+task.wait(2)
+game:GetService("StarterGui"):SetCore("SendNotification",{
+    Title = "Anarchy V2 | Light Mode", -- Required
+    Text = "Loading Metahooks...", -- Required
+        Duration = "5" -- Optional
+})
+task.wait(7)
+while true do end
