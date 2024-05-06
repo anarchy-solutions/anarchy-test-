a02f0f7243bdecf049a59c7cb1ad4d78b7372017
anarchy-solutions <164175212+anarchy-solutions@users.noreply.github.com>
2024-05-06 17:54:08 +0200

    Create anarchy-source.lua

---
+++ anarchy-source.lua
@@ -0,0 +1,21 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+    Title = "Anarchy V2 | BETA", -- Required
+    Text = "loaded", -- Required
+        Duration = "3" -- Optional
+})
+
+__index = __index
+__index = __index
+__index = function()
+ return __index
+end
+__index = __index
+
+
+while __index do
+  if __index == __index then
+    while __index == __index do
+      print"loaded"
+     end
+   end
+ end
